ab125c8a8bb6dd949a069e58f4b51fb164885927
juyeon_Prestige16 <zooy2on01@gmail.com>
2023-09-21 09:52:54 +0900

    반복문 안에 함수 호출 추가

--- character_circle_rect_move.py
+++ character_circle_rect_move.py
@@ -1,8 +1,15 @@
+#Drill02 캐릭터 사각 운동과 원 운동
+# 사각형 운동과 원 운동을 번갈아 가면서 무한 반복함
+
 from pico2d import*
 
 open_canvas()
 
 grass = load_image('glass.png')
 character = load_image('character.png')
+
+while True:
+    run_circle()
+    run_rectangle()
 
 close_canvas()
